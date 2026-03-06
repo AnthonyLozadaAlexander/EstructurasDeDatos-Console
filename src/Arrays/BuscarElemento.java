c28fa47050cfca7b4a08765bdb6ae0bd2a4e492b
zDefcon <antholozadarequelme@gmail.com>
2026-03-05 20:28:07 -0500

    Agrega clase para buscar elementos en un arreglo

---
+++ src/Arrays/BuscarElemento.java
@@ -0,0 +1,19 @@
+﻿package Arrays;
+
+public class BuscarElemento {
+    static void main() {
+        int[] numeros = {1,20,21,40,55,60}; // arreglo definido con 6 elementos
+        int buscar = 60; // elemento a buscars
+
+        for (int i = 0; i < numeros.length; i++) {
+            if(numeros[i] == buscar){
+                System.out.println("Elemento encontrado en la Pos["+i+"]");
+                break;
+            }
+            else{
+                System.out.println("Elemento No Existente en el arreglo");
+            }
+
+        }
+    }
+}
