83995aee987d0a5dfc3e0be7a5419b047e9e2a01
zDefcon <antholozadarequelme@gmail.com>
2026-03-05 20:35:53 -0500

    Corrige el método main para buscar elementos en un arreglo
    Se modifica el método main para que sea público y se agrega una variable que indica si el elemento fue encontrado. Además, se mejora la salida de la consola para mostrar el arreglo y el resultado de la búsqueda de manera más clara.

--- src/Arrays/BuscarElemento.java
+++ src/Arrays/BuscarElemento.java
@@ -1,17 +1,27 @@
-﻿package Arrays;
+package Arrays;
 
 public class BuscarElemento {
-    static void main() {
+    public static void main() {
         int[] numeros = {1,20,21,40,55,60}; // arreglo definido con 6 elementos
         int buscar = 60; // elemento a buscars
+        boolean encontrado = false; // indicar si se encontro el elemento
+
+        System.out.println("Arreglo De Numeros\n");
+        for(int i : numeros){
+            System.out.print("["+i+"]");
+        }
+
+        System.out.println("\n---------------------------------------------\n");
 
         for (int i = 0; i < numeros.length; i++) {
             if(numeros[i] == buscar){
                 System.out.println("Elemento encontrado en la Pos["+i+"]");
+                encontrado = true;
                 break;
             }
-            else{
-                System.out.println("Elemento No Existente en el arreglo");
+
+            if(!encontrado){
+                System.out.println("Elemento No Existente En El Arreglo");
             }
 
         }
