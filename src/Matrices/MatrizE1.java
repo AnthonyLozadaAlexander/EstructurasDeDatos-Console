4deb6edc801cbec73b2f422a38f0cda312929684
zDefcon <antholozadarequelme@gmail.com>
2026-04-10 02:49:43 -0500

    Agrega clase MatrizE1 con método main para inicializar una matriz

---
+++ src/Matrices/MatrizE1.java
@@ -0,0 +1,14 @@
+﻿package Matrices;
+
+import java.util.Scanner;
+
+public class MatrizE1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int f, c;
+        System.out.println("Ingrese el Tamaño de la Matriz (f x c)");
+        System.out.print("n -> "); f = input.nextInt();
+        System.out.print("c -> "); c = input.nextInt();
+        int[][] matriz = new int[f][c];
+    }
+}
