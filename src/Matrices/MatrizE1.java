b99b2dabcac14143f14df7812f6eb8cb14a1d671
zDefcon <antholozadarequelme@gmail.com>
2026-04-10 03:15:52 -0500

    Agrega funcionalidad para ingresar y mostrar elementos de la matriz Se implementa un bucle que permite al usuario ingresar los elementos de una matriz de tamaño definido por el usuario. Además, se agrega un método para mostrar la matriz en consola.

--- src/Matrices/MatrizE1.java
+++ src/Matrices/MatrizE1.java
@@ -9,12 +9,25 @@ public class MatrizE1 {
         System.out.println("Ingrese el Tamaño de la Matriz (f x c)");
         System.out.print("n -> "); f = input.nextInt();
         System.out.print("c -> "); c = input.nextInt();
+
         int[][] matriz = new int[f][c];
         for (int i = 0; i < f ; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.println("Ingrese el Elemento["+i+"]["+j+"]");
+                System.out.print("-> ");
+                matriz[i][j] = input.nextInt();
+            }
+        }
+
+    }
+
+    public void mostrarMatriz(int[][] matriz, int f, int c]){
+        for (int i = 0; i < f; i++) {
             for (int j = 0; j < c; j++) {
 
             }
 
         }
+
     }
 }
