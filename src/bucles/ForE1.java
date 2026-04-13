3fb7806dc09f0e744bb658fbf2bd4f8a1cad4737
zDefcon <antholozadarequelme@gmail.com>
2026-04-13 04:25:15 -0500

    Agrega método main y función sumaN en la clase ForE1
    Se implementa el método main que solicita un número al usuario y llama a la función sumaN.
    La función sumaN calcula la suma de un contador hasta el número ingresado.

--- src/bucles/ForE1.java
+++ src/bucles/ForE1.java
@@ -1,7 +1,28 @@
 ﻿package bucles;
 
-public class ForE1 {
-    static void main(String[] args) {
+import java.util.Scanner;
 
+public class ForE1 {
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+        int z = 0, n=0;
+        System.out.println("------------------------------");
+        System.out.println("       Ejercicio For");
+        System.out.println("------------------------------");
+        System.out.println("Ingrese n: ");
+        n = input.nextInt();
+        input.nextLine(); // limpiar buffer
+        System.out.println("------------------------------");
+        int res = sumaN(n, z);
+
+    }
+
+    public static int sumaN(int n, int z){
+
+        for (int i = 0; i < n; i++) {
+            z = z + 1;
+        }
+
+        return z;
     }
 }
