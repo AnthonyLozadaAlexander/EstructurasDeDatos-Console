a3bdc94903f4ce177461ad61b4016d7de7196761
zDefcon <antholozadarequelme@gmail.com>
2026-04-13 05:07:37 -0500

    Agrega clase ArrayComparacion con métodos para ingresar y mostrar notas

---
+++ src/Arrays/ArrayComparacion.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ArrayComparacion {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] notas; int n; int i = 0;
+        System.out.print("Ingrese el numero de notas a ingresar: ");
+        n = input.nextInt(); notas = new int[n];
+        ingresarNotas(notas, input);
+        mostrarNotas(notas);
+    }
+
+    public static int[] ingresarNotas(int[] notas, Scanner input){
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("Ingrese la nota["+i+"]");
+            notas[i] = input.nextInt();
+        }
+        return notas;
+    }
+
+    public static void mostrarNotas(int[] notas){
+        int i = 0;
+        while(i < notas.length){
+            System.out.println("La nota["+i+"]: " + notas[i]);
+            i++;
+        }
+    }
+
+}
