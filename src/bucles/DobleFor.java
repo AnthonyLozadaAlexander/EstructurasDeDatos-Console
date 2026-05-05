81dd4b95c19ac6f6daf715f51f78045806b8b153
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-05 00:55:49 -0500

    Agrega clase DobleFor con método para calcular BigO

---
+++ src/bucles/DobleFor.java
@@ -0,0 +1,19 @@
+package bucles;
+
+class DobleFor {
+    static void main(String[] args) {
+        int n = 6;
+        System.out.println("El resultado de la funcion BigO es: " + BigO(n));
+    }
+
+    static int BigO(int n){
+        int z = 0;
+        for (int i = n; i > 1; i /= 2) {
+            for (int j = 1; j <= n; j++) {
+                z++;
+            }
+        }
+        return z;
+    }
+}
+
