3a35c12ecd1a9ae6765623cf30c1177391a8ee25
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-07 01:43:16 -0500

    Agrega clase MatrizE3 para ingresar elementos en una matriz

---
+++ src/Matrices/MatrizE3.java
@@ -0,0 +1,19 @@
+﻿package Matrices;
+
+import java.util.Scanner;
+
+public class MatrizE3 {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i, j, mayor = 0;
+        int[][] matriz = new int[3][3];
+        for (i = 0; i < matriz.length; i++) {
+            for (j = 0; j < matriz[i].length; j++){
+                System.out.print("Ingrese El Elemento De ["+i+"]["+j+"]");
+                matriz[i][j] = input.nextInt();
+            }
+        }
+    }
+
+
+}
