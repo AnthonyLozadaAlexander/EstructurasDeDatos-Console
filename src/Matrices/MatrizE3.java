2b900d1141fc04a612b00b31ad8781120ca12e8a
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-07 01:49:13 -0500

    Agrega método imprimirMatriz para mostrar elementos de la matriz

--- src/Matrices/MatrizE3.java
+++ src/Matrices/MatrizE3.java
@@ -15,5 +15,15 @@ public class MatrizE3 {
         }
     }
 
+    public void imprimirMatriz(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++){
+                if(i == 0 && j == 0) System.out.print("[ " + matrix[i][j] + " ");
+                System.out.print(matrix[i][j] + " ");
+                if(i == matrix.length - 1 && j == matrix[i].length - 1) System.out.print( matrix[i][j] + " ]");
+            }
+        }
+    }
+
 
 }
