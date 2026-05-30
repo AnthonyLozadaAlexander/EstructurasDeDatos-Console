0975d3606044a236f2da51c1f68f8c9b479b75b9
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-29 23:48:16 -0500

    Mejora la impresión de la matriz al agregar formato y separación - Agrega un espacio al mensaje de entrada de elementos. - Modifica la impresión de la matriz para incluir corchetes y saltos de línea adecuados.

--- src/Matrices/MatrizE3.java
+++ src/Matrices/MatrizE3.java
@@ -1,4 +1,4 @@
-﻿package Matrices;
+package Matrices;
 
 import java.util.Scanner;
 
@@ -9,7 +9,7 @@ public class MatrizE3 {
         int[][] matriz = new int[3][3];
         for (i = 0; i < matriz.length; i++) {
             for (j = 0; j < matriz[i].length; j++){
-                System.out.print("Ingrese El Elemento De ["+i+"]["+j+"]");
+                System.out.print("Ingrese El Elemento De ["+i+"]["+j+"]: ");
                 matriz[i][j] = input.nextInt();
             }
         }
@@ -19,11 +19,15 @@ public class MatrizE3 {
 
     public static void imprimirMatriz(int[][] matrix){
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++){
-                if(i == 0 && j == 0) System.out.print("[ " + matrix[i][j] + " \n");
-                System.out.print(matrix[i][j] + " ");
-                if(i == matrix.length - 1 && j == matrix[i].length - 1) System.out.print("\n" + matrix[i][j] + " ]");
+            if(i == 0){
+                System.out.print("[ ");
             }
+            for (int j = 0; j < matrix[i].length; j++){
+                System.out.print(matrix[i][j] + " ");
+                if(i == matrix.length - 1 && j == matrix[i].length - 1) System.out.print(" ]");
+            }
+
+            System.out.println();
         }
     }
 
