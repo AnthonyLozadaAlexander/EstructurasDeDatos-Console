df9713d356b92fceaa0cf3d0b02410da1d6ccc27
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-30 00:14:06 -0500

    Agrega lógica para encontrar el mayor elemento en la matriz
    Se implementa un bucle que recorre la matriz ingresada por el usuario para identificar el mayor valor y sus índices. Esto mejora la funcionalidad de la clase MatrizENumMayor al permitir no solo la entrada de datos, sino también el análisis de los mismos.

--- src/Matrices/MatrizENumMayor.java
+++ src/Matrices/MatrizENumMayor.java
@@ -8,6 +8,8 @@ public class MatrizENumMayor {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int mayor = 0;
+        int indexF = 0;
+        int indexC = 0;
         int[][] matriz = new int[3][3];
 
         for (int i = 0; i < matriz.length; i++) {
@@ -15,11 +17,24 @@ public class MatrizENumMayor {
                 System.out.print("Digite un valor["+i+"]["+j+"]: ");
                 matriz[i][j] = input.nextInt();
                 input.nextLine(); // limpiar buffer
+
             }
 
         }
 
         AlgoritmoMatriz.imprimirMatriz(matriz);
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if(matriz[i][j] > mayor){
+                    mayor = matriz[i][j];
+                    indexF = i;
+                    indexC = j;
+                }
+
+            }
+
+        }
 
     }
 }
